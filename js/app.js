c4cddb633766d0036e6fac463357a7b81fdb5bb1
“Wai <waye14671@gmail.com>
2026-01-17 09:57:08 +0630

    initial commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+
+
+function add(a,b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    if(a > b){
+        return a - b ;
+    }else {
+        return b - a;
+    }
+}
+
+function show(){
+
+}
+
+function hide(){
+
+}
+
+function hello(){
+
+}
